68d09199db1b159ffde418285b84764b4d500def
Tjdms16 <98seoeun1@gmail.com>
2023-01-12 21:52:00 +0900

    제품 리스트 + 페이징 + 필터링 구현 중

--- src/main/java/com/theharmm/sale/SaleVo.java
+++
@@ -1,5 +0,0 @@
-package com.theharmm.sale;
-
-public class SaleVo {
-
-}
--- src/main/java/com/theharmm/service/ProductService.java
+++ src/main/java/com/theharmm/service/ProductService.java
@@ -1,5 +1,25 @@
 package com.theharmm.service;
 
-public class ProductService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.theharmm.mapper.ProductMapper;
+import com.theharmm.vo.product.Criteria;
+import com.theharmm.vo.product.ProductPageDTO;
+import com.theharmm.vo.product.ProductVo;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Service
+@AllArgsConstructor
+public class ProductService {
+	private ProductMapper mapper;
+	
+	// 제품 코드로 제품 별 색상 가져오기
+	public List<ProductPageDTO> getAllProduct(Criteria cri) {
+		/* return mapper.selectAllProducts(); */
+	}
 }
