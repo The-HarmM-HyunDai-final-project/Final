5d1cb74a7e04877df9588dfdbc1980188f695c06
shinmirim <90263843+shinmirim@users.noreply.github.com>
2023-01-12 23:41:04 +0900

    프로젝트 구조 조정 및 상품상세 페이지 띄우기 완료

--- src/main/java/com/theharmm/service/ProductService.java
+++ src/main/java/com/theharmm/service/ProductService.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.theharmm.dto.product.Criteria;
+import com.theharmm.dto.product.ProductPageDTO;
+import com.theharmm.dto.product.ProductVo;
 import com.theharmm.mapper.ProductMapper;
-import com.theharmm.vo.product.Criteria;
-import com.theharmm.vo.product.ProductPageDTO;
-import com.theharmm.vo.product.ProductVo;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
